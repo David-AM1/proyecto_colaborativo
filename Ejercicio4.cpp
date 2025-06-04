84b436701068962d6e06d82681520acf75081ffe
Matias <mhumpirig@unjbg.edu.pe>
2025-06-03 22:19:49 -0500

    En lugar de usar if,if else y else, se optó por hacer un int mayor y usar dos if con las otras variables

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
 int mayorDeTres(int a, int b, int c) {
-    if (a >= b && a >= c)
-        return a;
-    else if (b >= a && b >= c)
-        return b;
-    else
-        return c;
+    int mayor = a;
+    if (b > mayor) mayor = b;
+    if (c > mayor) mayor = c;
+    return mayor;
 }
 int main() {
     int x, y, z;
