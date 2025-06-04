460ca9dbc8141b3d9207d1498b3fa97b39281318
David-AM1 <davalosm@unjbg.edu.pe>
2025-06-03 21:56:48 -0500

    Se agrega validaciones para datos ingresados que no sean validos en el EJERCICIO 1 (SECUENCIALES)

--- 1. Perimetro (SECUENCIAL).cpp
+++ 1. Perimetro (SECUENCIAL).cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 int main() {
 	double perimetro, altura, base;
-	cout<<"Ingrese la altura del rectangulo: ";
-	cin>>altura;
-	cout<<"Ingrese la base del rectangulo: ";
-	cin>>base;
+	do{
+		cout<<"Ingrese la altura del rectangulo: ";
+		cin>>altura;
+		if(altura<=0){
+			cout<<"Numero invalido. Vuelva a intentar."<<endl;
+		}
+	}while(altura<=0);
+	do{
+		cout<<"Ingrese la base del rectangulo: ";
+		cin>>base;
+		if(base<=0){
+			cout<<"Numero invalido. Vuelva a intentar."<<endl;
+		}
+	}while(base<=0);
 	perimetro = (base + altura) * 2;
 	cout<< "El perimetro del rectangulo es: "<< perimetro;
 	return 0;
