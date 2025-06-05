52e5e748c105e05fc5784d6f28dca7edd84d96f1
David-AM1 <davalosm@unjbg.edu.pe>
2025-06-04 20:28:01 -0500

    Se agrega la oportunidad de volver a intentar con nuevos valores en el EJERCICIO 4

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -8,14 +8,19 @@ int mayorDeTres(int a, int b, int c) {
 }
 int main() {
     int x, y, z;
+    char respuesta;
+    do{
     cout << "Ingrese el valor de x: ";
     cin >> x;
-        cout << "Ingrese el valor de y: ";
+    cout << "Ingrese el valor de y: ";
     cin >> y;
-        cout << "Ingrese el valor de z: ";
+    cout << "Ingrese el valor de z: ";
     cin >> z;
     int mayor = mayorDeTres(x, y, z);
     cout << "El mayor es: " << mayor << endl;
+    cout<<"¿Desea volver a intentar con otros valores? (s/n): ";
+    cin >> respuesta;
+	}while(respuesta=='s');
     return 0;
 }
 
