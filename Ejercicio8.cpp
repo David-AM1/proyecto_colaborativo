8cbdc21da5b2074848eb9df56b2396876d9eb390
Matias <mhumpirig@unjbg.edu.pe>
2025-06-09 13:37:38 -0500

    En lugar de usar una variable resultado, podemos directamente actualizar a y b en el bucle, así simplificamos el código

--- Ejercicio8.cpp
+++ Ejercicio8.cpp
@@ -5,11 +5,9 @@ int fibonacci(int n) {
     if (n == 1) return 1;
     int a = 0;
     int b = 1;
-    int resultado;
     for (int i = 2; i <= n; i++) {
-        resultado = a + b;
-        a = b;
-        b = resultado;
+        b = b + a;
+        a = b - a;
     }
     return b;
 }
