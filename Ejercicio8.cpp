ce425b5ef69c3b5f63e7c3017a82597419ca084e
Matias <mhumpirig@unjbg.edu.pe>
2025-06-09 13:35:33 -0500

    Agregando Ejercicio8.cpp

---
+++ Ejercicio8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int a = 0;
+    int b = 1;
+    int resultado;
+    for (int i = 2; i <= n; i++) {
+        resultado = a + b;
+        a = b;
+        b = resultado;
+    }
+    return b;
+}
+int main() {
+    int n;
+    cout << "Ingrese el valor de n: ";
+    cin >> n;
+    int resultado = fibonacci(n);
+    cout << "El numero de Fibonacci en la posicion " << n << " es: " << resultado << endl;
+    return 0;
+}
+
